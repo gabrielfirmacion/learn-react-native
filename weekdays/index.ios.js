57ed8bf20a02a4b3a3a50f3e920c4c944407094d
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-01 17:50:03 -0700

    fix styling of weekdays app

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -1,4 +1,5 @@
 // Import some code we need
+var Moment = require('moment');
 var React = require('react');
 var ReactNative = require('react-native');
 var AppRegistry = ReactNative.AppRegistry;
@@ -7,22 +8,24 @@ var View = ReactNative.View;
 var StyleSheet = ReactNative.StyleSheet;
 var DayItem = require('./src/day-item')
 
-var DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 // Create a react component
 var Weekdays = React.createClass({
   render: function() {
     return <View style={styles.container}>
-      <Text>
-        Days of the week:
-      </Text>
       {this.days()}
     </View>
   },
   days: function() {
-    return DAYS.map(function(day){
-      // Called 7 times, one for each day of the week
-      return <DayItem day={day} />
-    });
+    var daysItems = [];
+
+    for (var i=0; i<7; i++){
+      var day = Moment().add(i, 'days').format('dddd');
+      daysItems.push(
+        <DayItem day={day} daysUntil={i} />
+      )
+    }
+
+    return daysItems
   }
 });
 
--- weekdays/src/day-item.js
+++ weekdays/src/day-item.js
@@ -2,22 +2,35 @@
 var React = require('react');
 var ReactNative = require('react-native');
 var Text = ReactNative.Text;
-var StyleSheet = ReactNative.StyleSheet;
 
 // create our component
 var DayItem = React.createClass({
   render: function() {
-    return <Text style={styles.day}>
+    return <Text style={this.style()}>
       {this.props.day}
     </Text>
-  }
-});
-
-// style our component
-var styles = StyleSheet.create({
-  day: {
-    fontSize: 18,
-    color: '#0000FF'
+  },
+  style: function() {
+    return {
+      color: this.color(),
+      fontWeight: this.fontWeight(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight()
+    }
+  },
+  color: function() {
+    var opacity = 1 / this.props.daysUntil;
+    return 'rgba(0, 0, 0, '+opacity+')';
+  },
+  fontWeight: function() {
+    var weight = 7 - this.props.daysUntil;
+    return (weight * 100).toString();;
+  },
+  fontSize: function() {
+    return 60 - 6 * this.props.daysUntil
+  },
+  lineHeight: function() {
+    return 70 - 4 * this.props.daysUntil;
   }
 });
 
