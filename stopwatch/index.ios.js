1e22016590bc79ca455906f7ca7ab3133eb8e185
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-02 15:44:55 -0700

    style laps

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -39,11 +39,11 @@ var StopWatch = React.createClass({
   },
   laps: function() {
     return this.state.laps.map(function(time, index){
-      return <View>
-        <Text>
+      return <View style={styles.lap}>
+        <Text style={styles.lapText}>
           Lap #{index + 1}
         </Text>
-        <Text>
+        <Text style={styles.lapText}>
           {formatTime(time)}
         </Text>
       </View>
@@ -130,6 +130,13 @@ var styles = StyleSheet.create({
   },
   stopButton: {
     borderColor: '#CC0000'
+  },
+  lap: {
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  },
+  lapText: {
+    fontSize: 30
   }
 });
 
