b25a740bdcb41f2cb0e5255492a0b5bd02fa3fab
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-03 13:05:28 -0700

    set up environment for both ios and android

---
+++ authentication/index.ios.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactNative = require('react-native');
+var { AppRegistry } = ReactNative;
+
+var Main = require('./src/main');
+
+AppRegistry.registerComponent('authentication', () => Main);
