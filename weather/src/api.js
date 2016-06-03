bdd459bd29df177c527821396f97a25348ea36f8
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-03 10:42:59 -0700

    add text wrapper to map and require lodash

--- weather/src/api.js
+++ weather/src/api.js
@@ -1,5 +1,21 @@
+var _ = require('lodash');
 var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=ac6707379a8b0c658ae10abd6555e347'
 
-module.exports = function(latitude, longtitude) {
-  var url = '${rootUrl}&lat=${latitude}&lon=${longtitude}'
+var KelvinToF = function(kelvin) {
+  return Math.round((kelvin - 273.5) * 1.8 + 32) + ' ˚F'
+}
+module.exports = function(latitude, longitude) {
+  var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+
+  return fetch(url)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(json){
+      return {
+        city: json.name,
+        temperature: KelvinToF(json.main.temp),
+        description: _.capitalize(json.weather[0].description)
+      }
+    });
 }
