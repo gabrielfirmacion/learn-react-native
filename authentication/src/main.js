8b0ac8fef4fbf0513d5ecb858793c42e64d0534f
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-03 16:59:10 -0700

    setup environment for routes

--- authentication/src/main.js
+++ authentication/src/main.js
@@ -1,33 +1,39 @@
 var React  = require('react');
 var ReactNative = require('react-native');
 var {
-  View,
-  Text,
-  StyleSheet
+  StyleSheet,
+  Navigator
 } = ReactNative;
 
 var Parse = require('parse/react-native');
-
 var Signin = require('./components/authentication/signin');
+
+var ROUTES = {
+  signin: Signin
+};
 
 module.exports = React.createClass({
   componentWillMount: function() {
     Parse.initialize('myAppId','unused');
     Parse.serverURL = 'https://authentication-react-native.herokuapp.com/parse';
   },
+  renderScene: function(route, navigator) {
+    var Component = ROUTES[route.name];
+    return <Component />;
+  },
   render: function() {
     return (
-      <View style={styles.container}>
-        <Signin />
-      </View>
-    )
+      <Navigator
+        style={styles.container}
+        initialRoute={{name: 'signin'}}
+        renderScene={this.renderScene}
+        configureScene={() => { return Navigator.SceneConfigs.FloatFromRight; }} />
+    );
   }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1
   }
 });
