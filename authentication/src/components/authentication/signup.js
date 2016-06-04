c5764c4d073fe70ddcb6528069ad44c34e94d8ee
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-06-03 17:36:35 -0700

    create sign up form and button to take user back to signin

--- authentication/src/components/authentication/signup.js
+++ authentication/src/components/authentication/signup.js
@@ -4,15 +4,48 @@ var {
   View,
   Text,
   StyleSheet,
+  TextInput
 } = ReactNative;
 
+var Button = require('../common/button');
+
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+  },
   render: function() {
     return (
       <View style={styles.container}>
-        <Text>Sign in Here</Text>
+        <Text>Sign Up</Text>
+
+        <Text style={styles.label}>Username:</Text>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(text) => this.setState({username: text})}
+          style={styles.input} />
+
+        <Text style={styles.label}>Password:</Text>
+        <TextInput
+          value={this.state.password}
+          onChangeText={(text) => this.setState({password: text})}
+          style={styles.input} />
+
+        <Text style={styles.label}>Confirm:</Text>
+        <TextInput
+          value={this.state.passwordConfirmation}
+          onChangeText={(text) => this.setState({passwordConfirmation: text})}
+          style={styles.input} />
+
+          <Button text={'I have an account...'} onPress={this.onSigninPress} />
       </View>
     )
+  },
+  onSigninPress: function() {
+    this.props.navigator.pop();
   }
 });
 
@@ -22,5 +55,18 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white'
+  },
+  label: {
+    fontSize: 18
+  },
+  input: {
+    padding: 4,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 200,
+    alignSelf: 'center'
   }
 });
